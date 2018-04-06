80a319cbedcbfb33915d455cdd4d48df19f79e4b
Sabrina Kennedy <skenne21@gmail.com>
2018-04-06 13:27:43 -0600

    tests for cleaners, not passing

--- src/mockData/index.js
+++ src/mockData/index.js
@@ -1,3 +1,25 @@
+export const rawVeido = [{
+  id:{
+    kind: "youtube#video", videoId: "XcVpMJp9Th4"
+  },
+  snippet: {
+    channelId:"UC3OhT2ErtjLhvvkULJCCBlw",
+    description:"Elon Musk gives...",
+    publishedAt:"2017-09-29T06:05:03.000Z",
+    thumbnails:{
+      high: {
+        url:"https://i.ytimg.com/vi/XcVpMJp9Th4/hqdefault.jpg",
+        width: 480,
+      }, 
+      medium: {
+        url:"https://i.ytimg.com/vi/XcVpMJp9Th4/mqdefault.jpg",
+        width: 320,
+      },
+    },
+    title:"Elon Musk Gives...",
+  }
+}]
+
 export const veido = {
   id: "XcVpMJp9Th4",
   title: "Elon Musk",
@@ -29,7 +51,29 @@ export const veidos = [{
     url: "https://i.ytimg.com/vi/0PWu3BRxn60/hqdefault.jpg",
     width: 480,
     height: 360
+  } 
 }]
+
+export const rawBio = {
+  ceo:"Elon Musk",
+  coo:"Gwynne Shotwell",
+  cto:"Elon Musk",
+  cto_propulsion:"Tom Mueller",
+  employees:7000,
+  founded:2002,
+  founder:"Elon Musk",
+  headquarters: {
+    address: "Rocket Road",
+    city: "Hawthorne",
+    state: "California"
+  },
+  launch_sites:3,
+  name:"SpaceX",
+  summary:"SpaceX designs, manufactures...",
+  test_sites:1,
+  valuation:15000000000,
+  vehicles:3,
+}
 
 export const bio = [{
   name: "SpaceX",
