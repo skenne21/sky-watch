46194b42b7feeb60ed8b2a92d0d36efc43f8df9b
Sabrina Kennedy <skenne21@gmail.com>
2018-04-06 15:05:57 -0600

    Passing all actions tests

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -4,17 +4,17 @@ import * as mocks from '../mockData';
 describe('Actions', () =>{
   
   it('Should add a company bio', () => {
-    const bio = mocks.bio
+    const bio = mocks.bio;
     const expected = {
       type: 'ADD_COMPANY_BIO',
       bio
-    }
+    };
 
     expect(actions.addCompanyBio(bio)).toEqual(expected)
   });
 
   it('Should add an array of videos', () => {
-    const videos = mocks.veidos
+    const videos = mocks.veidos;
     const expected = {
       type: 'ADD_VIDEOS',
       videos
@@ -24,7 +24,12 @@ describe('Actions', () =>{
   });
 
   it('Should add a error message', () => {
-     
+    const message = 'Error happend';
+    const expected = {
+      type: 'ADD_ERROR',
+      message
+    };
+    expect(actions.addError(message)).toEqual(expected)
   });
 
 
