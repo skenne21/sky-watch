90e0bdb6f5f5c519390de805722887af87718aed
Sabrina Kennedy <skenne21@gmail.com>
2018-04-07 19:03:20 -0600

    Finish setup for capsules

--- src/containers/CapsulesContainer/CapsulesContainer.js
+++ src/containers/CapsulesContainer/CapsulesContainer.js
@@ -5,8 +5,25 @@ import Capsule from '../../components/Capsule/Capsule';
 
 
 export class CapsulesContainer extends Component {
-
+  
   createComponent = () => {
+    if(this.props.missionVideos.length) {
+      const { id, title, description } = this.props.missionVideos[2]
+      return (
+        <div>
+          <h2>{title}</h2>
+          <p>{description}</p>
+          <iframe src={`https://www.youtube.com/embed/${id}`}/>
+          {  
+            this.createCapsules()
+          }
+        </div>
+      )
+    }
+  }
+
+ 
+  createCapsules = () => {
     const { capsules } = this.props;
     return capsules.map((capsule, index) => {
       return <Capsule key={capsule.name+index} capsule={capsule} />
@@ -22,8 +39,9 @@ export class CapsulesContainer extends Component {
   }
 }
 
-export const mapStateToProps = ({ capsules }) => ({
-  capsules
+export const mapStateToProps = ({ capsules, missionVideos }) => ({
+  capsules,
+  missionVideos
 });
 
 export default withRouter(connect(mapStateToProps, null)(CapsulesContainer))
