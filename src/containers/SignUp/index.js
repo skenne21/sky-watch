83f7d13ef10f83245dc9bd1f9d2e428c69e8354f
Sabrina Kennedy <skenne21@gmail.com>
2018-04-11 20:03:26 -0600

    Add tests for signup

--- src/containers/SignUp/index.js
+++ src/containers/SignUp/index.js
@@ -87,11 +87,11 @@ export class SignUp extends Component {
   }
 }
 
-const mapStateToProps = ({user}) => ({
+export const mapStateToProps = ({user}) => ({
   user
 })
 
-const mapDispatchToProps = dispatch => ({
+export const mapDispatchToProps = dispatch => ({
   addUser: user => dispatch(actions.addUser(user))
 })
 
---
+++ src/containers/SignUp/index.test.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { mapStateToProps, mapDispatchToProps } from './index';
+import * as actions from '../../actions';
+import * as mocks from '../../mockData';
+import SignUp from './index';
+
+describe('SignUp', () => {
+  let wrapper, user, addUser, history;
+
+  beforeEach(() => {
+    history = {};
+    addUser= jest.fn();
+    user = { name: 'bob', id: 1, email: 'h@aol.com'};
+
+    wrapper = shallow(
+      <SignUp 
+        history={history}
+        addUser={addUser}
+        user={user}
+      />
+    );
+  });
+
+  it('Should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it.skip("Should have a default state of name as a empty string", () => {
+    expect(wrapper.state('name')).toEqual('');
+  });
+
+   describe('mapStateToProps', () => {
+    it('Should map user to state', () => {
+      const mockedState = { user: user };
+      const expected = { user: user };
+      const mapped = mapStateToProps(mockedState);
+      expect(mapped).toEqual(expected);
+    });
+  });
+
+  describe('mapDispatchToProps', () => {
+    it('Should dispatch addUser with the right params', () => {
+      const mockedDispatch = jest.fn();
+      const mapped = mapDispatchToProps(mockedDispatch);
+
+      mapped.addUser(user);
+      const expected = actions.addUser(user);
+
+      expect(mockedDispatch).toHaveBeenCalledWith(expected);
+    });
+  });
+
+});
