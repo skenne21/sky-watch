b19c8af0cf071f180b7c240d13a1d10edb574c72
Sabrina Kennedy <skenne21@gmail.com>
2018-04-11 19:05:00 -0600

    Add tests to homepage

---
+++ src/containers/Homepage/index.test.js
@@ -0,0 +1,177 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import * as apiCalls from '../../helpers/apiCalls';
+import * as actions from '../../actions';
+import { mapStateToProps, mapDispatchToProps } from './index';
+import Homepage from './index';
+import * as mocks from '../../mockData';
+
+describe('Homepage Container', () => {
+  let wrapper;
+  let getRockets;
+  let getCapsules;
+  let getLaunchPads;
+  let handleError;
+  let companyBio;
+  let missionVideos;
+  let capsules;
+  let launchpads;
+
+  beforeEach(() => {
+    getRockets = jest.fn();
+    getCapsules = jest.fn();
+    getLaunchPads = jest.fn();
+    handleError=  jest.fn();
+    companyBio=[];
+    missionVideos =[];
+    capsules= [];
+    launchpads= [];
+
+    wrapper = shallow(
+      <Homepage
+        getRockets={getRockets}
+        getCapsules={getCapsules}
+        getLaunchPads={getLaunchPads}
+        handleError={handleError}
+        companyBio={companyBio}
+        missionVideos={missionVideos}
+        capsules={capsules}
+        launchpads={launchpads} 
+      />
+    );
+  });
+
+  it('Should Match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  })
+
+  it.skip('Should call handleRockets when clicked and pass correct params', () => {
+
+  });
+
+  it.skip('Should call handleCapsules when clicked and pass correct params', () => {
+    
+  });
+
+  it.skip('Should call handleLaunchpads when clicked and pass correct params', () => {
+    
+  });
+
+  describe('mapStateToProps', () => {
+    let mockedState, expectedState;
+    beforeEach(() => {
+      mockedState = {
+        companyBio: [],
+        missionVideos: [],
+        rockets: [],
+        capsules: [],
+        launchpads: [],
+        user: {}
+      }
+
+      expectedState = {
+        companyBio: [],
+        missionVideos: [],
+        rockets: [],
+        capsules: [],
+        launchpads: [],
+        user: {}
+      }
+    })
+
+    it('should map companyBio to state', () => {
+      const bio = mocks.bio;
+      mockedState.companyBio = [bio];
+      const mapped = mapStateToProps(mockedState);
+      expectedState.companyBio = [bio];
+      expect(mapped).toEqual(expectedState);
+    });
+
+    it('Should map missionVideos to state', () => {
+      const videos = mocks.videos;
+      mockedState.missionVideos = videos;
+      const mapped = mapStateToProps(mockedState);
+      expectedState.missionVideos = videos;
+      expect(mapped).toEqual(expectedState);
+    });
+
+    it('Should map Rockets to state', () => {
+      const rockets = mocks.rockets;
+      mockedState.rockets = rockets;
+      const mapped = mapStateToProps(mockedState);
+      expectedState.rockets = rockets;
+      expect(mapped).toEqual(expectedState);
+    });
+
+    it('Should map capsules to state' , () => {
+      const capsules = mocks.capsules;
+      mockedState.capsules = capsules;
+      const mapped = mapStateToProps(mockedState);
+      expectedState.capsules = capsules;
+      expect(mapped).toEqual(expectedState);
+    });
+
+    it('Should map launchpads to state', () => {
+      const launchpads = mocks.launchpads;
+      mockedState.launchpads = launchpads;
+      const mapped = mapStateToProps(mockedState);
+      expectedState.launchpads = launchpads;
+      expect(mapped).toEqual(expectedState);
+    });
+
+    it('Should map user to state', () => {
+      const user = {
+        name:'bob',
+        id: 1,
+        email:'bob@gmail.com'
+      };
+
+      mockedState.user = user;
+      const mapped = mapStateToProps(mockedState);
+      expectedState.user = user;
+      expect(mapped).toEqual(expectedState);
+    });
+  });
+
+  describe('MapDispatchToProps', () => {
+    let mockedDispatch, mapped;
+
+    beforeEach(() => {
+      mockedDispatch = jest.fn();
+      mapped = mapDispatchToProps(mockedDispatch);
+    })
+
+    it('Should dispatch getRockets with the right params', () => {
+      const rockets = mocks.rockets;
+      mapped.getRockets(rockets);
+      const expected = actions.addRockets(rockets);
+
+      expect(mockedDispatch).toHaveBeenCalledWith(expected);
+    });
+
+    it('Should dispatch getCapsules with the right params', () => {
+      const capsules = mocks.capsules;
+      mapped.getCapsules(capsules);
+
+      const expected = actions.addCapsules(capsules);
+
+      expect(mockedDispatch).toHaveBeenCalledWith(expected);
+    });
+
+    it('Should dispatch getLaunchPads with the right params', () => {
+      const launchpads = mocks.launchpads;
+      mapped.getLaunchpads(launchpads);
+      const expected = actions.addLaunchPads(launchpads);
+
+      expect(mockedDispatch).toHaveBeenCalledWith(expected);
+    });
+
+    it('Should dispatch handleError with the right params', ( ) => {
+      const error = { message: 'error'};
+      mapped.handleError(error.message);
+
+      const expected = actions.addError(error.message);
+      expect(mockedDispatch).toHaveBeenCalledWith(expected);
+    })
+  });
+});
