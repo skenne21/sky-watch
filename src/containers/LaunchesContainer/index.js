db88a26dcd35a29d245b29e3a8d9420c1de4176a
Sabrina Kennedy <skenne21@gmail.com>
2018-04-13 20:48:05 -0600

    Add button functonality

--- src/containers/LaunchesContainer/index.js
+++ src/containers/LaunchesContainer/index.js
@@ -5,29 +5,49 @@ import * as actions from '../../actions';
 import PropTypes from 'prop-types';
 import Card from '../../components/Card';
 
-class LaunchesContainer extends Component {
-  constuctor() {
+export class LaunchesContainer extends Component {
+  constructor() {
     super();
     this.state = {
-
+      falcon1: [],
+      falcon9: [],
+      falconHeavy: [],
+      isActive: false,
     }
   }
 
+  handleClick = event => {
+    const { name } = event.target;
+    if (this.props.launches.length) {
+      if (!this.state[name].length) {
+        const launches = this.props.launches[0][name];
+        this.setState({ [name] : launches});
+        this.setState({isActive: true});
+      } else {
+        this.setState({isActive: false});
+      };
+    };
+  }
+
+
   render() {
     return (
-      <div clasName='LaunchesContainer'>
+      <div className='LaunchesContainer'>
         <button
+          name='falcon1'
           className='buttons'
           onClick={this.handleClick}
         >
           FALCON 1
         </button>
         <button
+          name='falcon9'
           className='buttons'
           onClick={this.handleClick}
         >
           FALCON 9</button>
         <button
+          name='falconHeavy'
           className='buttons'
           onClick={this.handleClick}
         >
@@ -37,8 +57,8 @@ class LaunchesContainer extends Component {
     )
   }
 }
-export const mapStateToProps = ({launches}) => {
+export const mapStateToProps = ({launches}) => ({
   launches
-}
+})
 
 export default withRouter(connect(mapStateToProps, null)(LaunchesContainer));
